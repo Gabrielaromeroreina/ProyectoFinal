5fd34b70cd0db3292825156ad8e34ee199b3c896
lvasquez5 <lauvasquez05@gmail.com>
2022-08-08 19:01:51 -0500

    signup changes

--- frontend/src/app/views/signup/signup.component.ts
+++ frontend/src/app/views/signup/signup.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+// libreria de captura de formularios de angular
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+
+// servicio de usuarios
+import { UserService } from 'src/app/services/user.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  constructor(public userService: UserService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  signup(form: NgForm) {
+    let { username, email, password, confirmPassword } = form.value;
+
+    //vericamos si los campos estan vacios
+    if (!username || !email || !password || !confirmPassword ) {
+      alert('Diligencie todos los campos');
+      return;
+    }
+
+    //verificamos si las contraseña coinciden
+    if (password !== confirmPassword) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+
+    this.userService.signUp({ username, email, password, confirmPassword }).subscribe(
+      (data: any) => {
+        alert(data.status);
+        form.reset();
+        this.userService.currentUser = new User();
+      },
+      (error) => {
+        alert(error.error.status);
+      }
+    );
   }
-
 }
+
